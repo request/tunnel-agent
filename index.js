56d26d526c9c50dc7cd8e64ced4cb6676ac2181b
hamper <hamper.nhs@gmail.com>
2019-06-30 02:50:43 +0300

    Destroy socket on unsuccessful CONNECT #46

--- index.js
+++ index.js
@@ -160,17 +160,18 @@ TunnelingAgent.prototype.createSocket = function createSocket(options, cb) {
   function onConnect(res, socket, head) {
     connectReq.removeAllListeners()
     socket.removeAllListeners()
-    self.sockets[self.sockets.indexOf(placeholder)] = socket
 
     if (res.statusCode === 200 && head.length == 0) {
       debug('tunneling connection has established')
+      self.sockets[self.sockets.indexOf(placeholder)] = socket
       cb(socket)
     } else {
       debug('tunneling socket could not be established, statusCode=%d', res.statusCode)
       var error = new Error('tunneling socket could not be established, ' + 'statusCode=' + res.statusCode)
       error.code = 'ECONNRESET'
-      cb(socket)
-      options.request.emit('error', error) // fire up error on ClientRequest.
+      options.request.emit('error', error)
+      socket.destroy()
+      self.removeSocket(placeholder)
     }
   }
 
